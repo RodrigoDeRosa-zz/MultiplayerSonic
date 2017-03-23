d201b9db664898d24ac39aedee3c823bed50b2d8
Martin Errazquin <martin.errazquin@hotmail.com>
2017-03-22 23:42:03 -0300

    logger 2.2: tiene log(LOG_ERROR, string) si se quiere mostrar info adicional

---
+++ logger/2-2/Logger2.hpp
@@ -0,0 +1,52 @@
+/*
+v2.0: Este tiene implementado adentro los mapas para .log(LOG_ERROR logerr)
+v2.1: Logea a DEFAULT_OUTPUT_PATH en vez de stdout       
+v2.2: Se agrega log(LOG_ERROR,string) para informacion adicional, si string es vacio asume log(LOG_ERROR)
+
+TODO:	-agregar mas funciones sobrecargadas de log (aunque hace falta mas que esto?)
+		
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <exception>
+
+using namespace std; 
+
+//utility
+enum LOGLV {BAJO,MEDIO,ALTO,LOGLV_TOTAL};	
+
+void timestamp(FILE* outstream,string mensaje);
+
+enum LOG_ERROR {JSON_NOT_FOUND,JSON_SYNTAX,LOG_ERROR_TOTAL}; //dejar TOTAL siempre al final
+
+//end:utility
+
+//CLASE LOGGER
+class Logger {
+	Logger();
+	LOGLV	log_level;
+	//funciones de inicio y fin del programa
+	void logProgStart();
+	void logProgFinish();
+
+public:
+	static Logger& getInstance();
+	void setLogLevel(LOGLV lv);
+	LOGLV getLogLevel();
+	//inicio y fin de funciones de logging con lo que reciben
+	void log(string str);	//string(generica)
+	void log(LOG_ERROR logerr);	//por código de error
+	void log(LOG_ERROR logerr,string addinfo); //codigo de error+info adicional
+	//void log(exception e);	//excepcion (hara falta?)
+
+
+private:
+	FILE* output_file;
+	map<LOG_ERROR,string> log_error_msg;	//mensaje correspondiente a cada error
+	map<LOG_ERROR,LOGLV> log_error_prior;	//nivel de detalle correspondiente a cada error
+	~Logger();	
+	void initMaps();
+};
+//end:CLASE LOGGER
