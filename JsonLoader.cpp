9c2da8dde8dc3e1754460607571b381351bf61aa
Guido Trucco <gftrucco@gmail.com>
2017-04-04 12:34:25 -0300

    agrego colores al json y mas chequeos

--- JsonLoader.cpp
+++ JsonLoader.cpp
@@ -18,18 +18,31 @@
 #define JSONLOADER_PARAM_NOT_FOUND_MSG "No se encontro el parametro "
 #define JSONLOADER_PARAM_NOT_INT_MSG "No se reconoce como entero el parametro "
 #define JSONLOADER_PARAM_NOT_POS_MSG "No es positivo el parametro "
+#define JSONLOADER_PARAM_NOT_STR_MSG "No se reconoce como cadena el parametro "
 #define DEFAULT_PATH "ejemplo.json"
+
 using namespace std;
 
+int verde[]= {0,204,0};
+int rojo[]= {204,0,0};
+int azul[]= {0,0,204};
+int amarillo[]= {255,255,0};
+int violeta[]= {102,0,102};
+
+
 JsonLoader::JsonLoader(char* ruta){
-	int verde[]= {0,204,0};
+	
 	colores["verde"] = verde;
+	colores["rojo"] = rojo;
+	colores["azul"] = azul;
+	colores["amarillo"] = amarillo;
+	colores["violeta"] = violeta;	
+	
 	ifstream in(ruta);
 	Json::Value json;
 
 	if(in.fail()){
 		Logger::getInstance().log("No se encontro el archivo .json",BAJO);
-		//in.close();
 		in.clear();
 		in.open(DEFAULT_PATH);
 	}
@@ -39,22 +52,20 @@ JsonLoader::JsonLoader(char* ruta){
 	this->stage = this->setStage(json);
 }
 
+
 Stage* JsonLoader::setStage(Json::Value json){
     Stage* stage = new Stage();
 
 	stage->setDimensiones(this->getPositiveInt(json["escenario"]["dimensiones"]["ancho"],"[escenario][dimensiones][ancho]",Window::getInstance().getWidth()*2),
 							this->getPositiveInt(json["escenario"]["dimensiones"]["alto"],"[escenario][dimensiones][alto]",Window::getInstance().getHeight()*2));
 
-
-
 	if(this->validateValue(json["escenario"]["capas"],"[escenario][capas]")){
 
 		int i = 0;
-
 		for(Json::Value::iterator it = json["escenario"]["capas"].begin(); it != json["escenario"]["capas"].end(); it++){
 			Layer* layer = new Layer();
 
-			layer->setTexPath(this->getString((*it)["ruta_imagen"]));
+			layer->setTexPath(this->getString((*it)["ruta_imagen"],"[escenario][capas][ruta_imagen]"));
 			layer->setDimensions( stage->getWidth(), stage->getHeight());
 			layer->loadImage();
 			layer->setIndexZ(this->getPositiveInt((*it)["index_z"],"[escenario][capas][index_z]",i));
@@ -63,7 +74,6 @@ Stage* JsonLoader::setStage(Json::Value json){
 			i++;
 		}
 	}
-
 
     SpriteGroup* activeSprites = this->getSprites(json);
 
@@ -83,48 +93,39 @@ SpriteGroup* JsonLoader::getSprites(Json::Value json){
 
 		Bloque* bloque;
 		Circulo* circulo;
-
-		if((*it)["circulo"].asBool()){
-			circulo = new Circulo(((*it)["coordenada"]["x"]).asInt(), ((*it)["coordenada"]["y"]).asInt(),
-			(*it)["radio"]["r"].asInt());
-			circulo->setTexture("Graficos/index.png");
-			activeSprites->add(circulo);
-		}
-		else{
-			bloque = new Bloque((*it)["coordenada"]["x"].asInt(), (*it)["coordenada"]["y"].asInt(),(*it)["dimensiones"]["alto"].asInt(),(*it)["dimensiones"]["ancho"].asInt());
-			bloque->setBackgroundColor(255,130,15);
-			activeSprites->add(bloque);}
-
-		/*int x = this->getPositiveInt((*it)["coordenada"]["x"],string("[escenario][entidades]") + string("[coordenada][x]"),-1);
+		
+		int x = this->getPositiveInt((*it)["coordenada"]["x"],string("[escenario][entidades]") + string("[coordenada][x]"),-1);
 		int y = this->getPositiveInt((*it)["coordenada"]["y"],string("[escenario][entidades]") +  string("[coordenada][y]"),-1);
-
+		
+		int* color = this->getColor((*it)["color"],"[escenario][entidades][color]");
+		
 		if(x<0 || y<0){
 			Logger::getInstance().log("No se creo el sprite");
 			continue;
 		}
 
-		sprite = new Bloque((*it)["coordenada"]["x"].asInt(), (*it)["coordenada"]["y"].asInt(),(*it)["dimensiones"]["ancho"].asInt(),(*it)["dimensiones"]["alto"].asInt());
-		sprite->setBackgroundColor(255,130,15);
-		sprite->setIndexZ((*it)["index_z"].asInt());
-		activeSprites->add(sprite);
-
 		if((*it)["circulo"].asBool()){
-			circulo = new Circulo(x, y,(*it)["radio"]["r"].asInt());
-			circulo->setTexture("Graficos/index.png");
+			int r = this->getPositiveInt((*it)["radio"],string("[escenario][entidades]") + string("[radio]"),-1);
+			if(r < 0){
+				Logger::getInstance().log("No se creo el sprite");
+				continue;
+			}
+			circulo = new Circulo(x, y, r);
+			circulo->setBackgroundColor(color[0],color[1],color[2]);
 			activeSprites->add(circulo);
 		}
 		else{
 			int h = this->getPositiveInt((*it)["dimensiones"]["alto"],string("[escenario][entidades]") + string("[dimensiones][alto]"),-1);
 			int w = this->getPositiveInt((*it)["dimensiones"]["ancho"],string("[escenario][entidades]") + string("[dimensiones][ancho]"),-1);
 			if(h<0 || w<0){
-			Logger::getInstance().log("No se creo el sprite");
-			continue;
-		}
-			bloque = new Bloque(x,y,h,w);
-			bloque->setBackgroundColor(255,130,15);
+				Logger::getInstance().log("No se creo el sprite");
+				continue;
+			}
+			bloque = new Bloque(x, y, h, w);
+			bloque->setBackgroundColor(color[0],color[1],color[2]);
 			activeSprites->add(bloque);
 		}
-		contadorEntidades++;*/
+		contadorEntidades++;
 	}
 
 	vector<Sprite*> vectorSprites = activeSprites->getSprites();
@@ -132,10 +133,15 @@ SpriteGroup* JsonLoader::getSprites(Json::Value json){
 	for (Json::Value::iterator it = json["escenario"]["texturas"].begin(); it != json["escenario"]["texturas"].end(); it++) {
 
 		Texture* texture = new Texture();
-		texture->loadFromFile((*it)["ruta"].asString());
+		texture->loadFromFile(getString((*it)["ruta"],"[escenario][texturas][ruta]"));
 
 		for(Json::Value::iterator idIt = (*it)["ids"].begin();idIt != (*it)["ids"].end();idIt++){
-			if((*idIt).asInt() >= vectorSprites.size())continue;
+			int spriteIndex = (*idIt).asInt();
+			if((*idIt).asInt() >= vectorSprites.size()) continue;
+			if(dynamic_cast<Circulo*>(vectorSprites[spriteIndex]) != NULL) {
+				((Circulo*)vectorSprites[spriteIndex])->setTexture((*it)["ruta"].asString());
+				continue;
+			}
 			vectorSprites[(*idIt).asInt()]->setTexture(texture);
 		}
 	}
@@ -149,7 +155,7 @@ void JsonLoader::setWindow(Json::Value json){
     Window::getInstance().init();
 }
 
-int JsonLoader::getPositiveInt(Json::Value json, string where, int defaultValue){
+int JsonLoader::getPositiveInt(Json::Value json, string where, int defaultValue, bool zero){
 	int value;
 	if(!(this->validateValue(json,where)) || !isInteger(json,where) || !isPositiveInteger(json,where)){
 		value = defaultValue;
@@ -163,27 +169,34 @@ bool JsonLoader::checkNullValue(Json::Value json){
 	return (json == Json::nullValue);
 }
 
-bool isPositiveInteger(Json::Value json, string where){
+bool JsonLoader::isPositiveInteger(Json::Value json, string where, bool zero){
 	if (json.asInt() > 0) return true;
+	if (zero && json.asInt() == 0) return true;
 	Logger::getInstance().log(string(JSONLOADER_PARAM_NOT_POS_MSG) + where,MEDIO);
 	return false;
 }
 
-bool isInteger(Json::Value json, string where){
+bool JsonLoader::isInteger(Json::Value json, string where){
 	if (json.isInt()) return true;
 	Logger::getInstance().log(string(JSONLOADER_PARAM_NOT_INT_MSG) + where,MEDIO);
 	return false;
 }
 
-string JsonLoader::getString(Json::Value json){
+string JsonLoader::getString(Json::Value json, string where, string defaultValue){
 
-	if(!json.isString() || (json.asString() == "")){
-		//handle
+	if(!this->isString(json,where)){
+		return defaultValue;
 	}
 	return json.asString();
 }
 
-bool fileExists(string path){
+bool JsonLoader::isString(Json::Value json, string where){
+	if (json.isString()) return true;
+	Logger::getInstance().log(string(JSONLOADER_PARAM_NOT_STR_MSG) + where,MEDIO);
+	return false;
+}
+
+bool JsonLoader::fileExists(string path){
 	return true;
 }
 
@@ -212,9 +225,13 @@ camara* JsonLoader::getCamara(){
 bool JsonLoader::validateValue(Json::Value json, string where){
 	bool valid = true;
 	if(this->checkNullValue(json)){
-		//printf("No se encontro la clave %s \n",where);
 		Logger::getInstance().log(string(JSONLOADER_PARAM_NOT_FOUND_MSG) + where,MEDIO);
 		valid = false;
 	}
 	return valid;
+}
+
+int* JsonLoader::getColor(Json::Value json, string where){
+	string color = this->getString(json,where,"verde");
+	int* colorRGB = colores[color];
 }
--- JsonLoader.hpp
+++ JsonLoader.hpp
@@ -9,24 +9,27 @@
 
 using namespace std;
 
-bool isPositiveInteger(Json::Value json, string where);
-bool isInteger(Json::Value json, string where);
-bool fileExists(string path);
+
 
 class JsonLoader{
 	Stage* stage;
 	map<string,int*> colores;
+	Stage* setStage(Json::Value json);
+	SpriteGroup* getSprites(Json::Value json);
+	void setWindow(Json::Value json);
+	void setRenderer();
+	int getPositiveInt(Json::Value json, string where, int defaultValue, bool zero = false);
+	bool checkNullValue(Json::Value json);
+	string getString(Json::Value json,string where,string defaultValue = string(""));
+	bool validateValue(Json::Value json, string where);
+	bool isPositiveInteger(Json::Value json, string where, bool zero = false);
+	bool isInteger(Json::Value json, string where);
+	bool fileExists(string path);
+	bool isString(Json::Value json, string where);
+	int* getColor(Json::Value json, string where);
 	public:
 		JsonLoader(char* ruta);
 		Stage* getStage();
 		camara* getCamara();
-	private:
-		Stage* setStage(Json::Value json);
-		SpriteGroup* getSprites(Json::Value json);
-		void setWindow(Json::Value json);
-		void setRenderer();
-		int getPositiveInt(Json::Value json, string where, int defaultValue);
-		bool checkNullValue(Json::Value json);
-		string getString(Json::Value json);
-		bool validateValue(Json::Value json, string where);
+		
 };
