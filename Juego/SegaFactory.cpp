0b4598cc0f4aa373230621f51664ca894141b51b
marcossch <schapiramarcos@gmail.com>
2017-04-25 20:44:26 -0300

    SONIC caminando, corriendo y esperando funcionan perfecto

--- Juego/SegaFactory.cpp
+++ Juego/SegaFactory.cpp
@@ -12,7 +12,7 @@ Sonic* SegaFactory::getSonic(string nomJugador){
       Sonic* sonic = new Sonic(nomJugador);
       //todos los sprites con sus texturas se tendrian que crear en el json loader
       /*creacion de el bloque */
-      MovingSonic* bloque = new MovingSonic(100, 200, 1500, 3000, 1);
+      MovingSonic* bloque = new MovingSonic(100, 300, 1500, 3000, 1);
       Texture* tex = new Texture();
   		tex->loadFromFile("Graficos/sonicSprites.png");
   		bloque->setTexture(tex);
@@ -28,36 +28,36 @@ Sonic* SegaFactory::getSonic(string nomJugador){
 void SegaFactory::initClips(MovingSonic* sonic){
 
   sonic->setClip("caminarDer");
-  sonic->setRectangulo("caminarDer",0,726,112,121);
-  sonic->setRectangulo("caminarDer",112,726,112,121);
-  sonic->setRectangulo("caminarDer",224,726,115,121);
-  sonic->setRectangulo("caminarDer",339,726,100,121);
-  sonic->setRectangulo("caminarDer",439,726,78,121);
-  sonic->setRectangulo("caminarDer",517,726,78,121);
-  sonic->setRectangulo("caminarDer",595,726,97,121);
-  sonic->setRectangulo("caminarDer",692,726,117,121);
-  sonic->setRectangulo("caminarDer",809,726,117,121);
-  sonic->setRectangulo("caminarDer",926,726,100,121);
-  sonic->setRectangulo("caminarDer",1026,726,79,121);
-  sonic->setRectangulo("caminarDer",1105,726,79,121);
-  sonic->setRectangulo("caminarDer",1184,726,85,121);
-  sonic->setRectangulo("caminarDer",1269,726,90,121);
+  sonic->setRectangulo("caminarDer",0,726,111,121);
+  sonic->setRectangulo("caminarDer",111,726,112,121);
+  sonic->setRectangulo("caminarDer",223,726,115,121);
+  sonic->setRectangulo("caminarDer",338,726,100,121);
+  sonic->setRectangulo("caminarDer",438,726,78,121);
+  sonic->setRectangulo("caminarDer",516,726,80,121);
+  sonic->setRectangulo("caminarDer",596,726,95,121);
+  sonic->setRectangulo("caminarDer",691,726,117,121);
+  sonic->setRectangulo("caminarDer",808,726,117,121);
+  sonic->setRectangulo("caminarDer",925,726,95,121);
+  sonic->setRectangulo("caminarDer",1020,726,78,121);
+  sonic->setRectangulo("caminarDer",1098,726,79,121);
+  sonic->setRectangulo("caminarDer",1177,726,85,121);
+  sonic->setRectangulo("caminarDer",1262,726,90,121);
 
   sonic->setClip("caminarIzq");
-  sonic->setRectangulo("caminarIzq",0,847,112,121);
-  sonic->setRectangulo("caminarIzq",112,847,112,121);
-  sonic->setRectangulo("caminarIzq",224,847,115,121);
-  sonic->setRectangulo("caminarIzq",339,847,100,121);
-  sonic->setRectangulo("caminarIzq",439,847,78,121);
-  sonic->setRectangulo("caminarIzq",517,847,78,121);
-  sonic->setRectangulo("caminarIzq",595,847,97,121);
-  sonic->setRectangulo("caminarIzq",692,847,117,121);
-  sonic->setRectangulo("caminarIzq",809,847,117,121);
-  sonic->setRectangulo("caminarIzq",926,847,100,121);
-  sonic->setRectangulo("caminarIzq",1026,847,79,121);
-  sonic->setRectangulo("caminarIzq",1105,847,79,121);
-  sonic->setRectangulo("caminarIzq",1184,847,85,121);
-  sonic->setRectangulo("caminarIzq",1269,847,90,121);
+  sonic->setRectangulo("caminarIzq",0,847,111,121);
+  sonic->setRectangulo("caminarIzq",111,847,112,121);
+  sonic->setRectangulo("caminarIzq",223,847,115,121);
+  sonic->setRectangulo("caminarIzq",338,847,100,121);
+  sonic->setRectangulo("caminarIzq",438,847,78,121);
+  sonic->setRectangulo("caminarIzq",516,847,80,121);
+  sonic->setRectangulo("caminarIzq",596,847,95,121);
+  sonic->setRectangulo("caminarIzq",691,847,117,121);
+  sonic->setRectangulo("caminarIzq",808,847,117,121);
+  sonic->setRectangulo("caminarIzq",925,847,95,121);
+  sonic->setRectangulo("caminarIzq",1020,847,78,121);
+  sonic->setRectangulo("caminarIzq",1098,847,79,121);
+  sonic->setRectangulo("caminarIzq",1177,847,85,121);
+  sonic->setRectangulo("caminarIzq",1262,847,90,121);
 
   sonic->setClip("quietoDer");
   sonic->setRectangulo("quietoDer",0,1452,92,121);
@@ -70,30 +70,30 @@ void SegaFactory::initClips(MovingSonic* sonic){
 
   sonic->setClip("quietoIzq");
   sonic->setRectangulo("quietoIzq",0,1573,92,121);
-  sonic->setRectangulo("quietoIzq",92,1573,97,121);
-  sonic->setRectangulo("quietoIzq",189,1573,97,121);
-  sonic->setRectangulo("quietoIzq",286,1573,91,121);
-  sonic->setRectangulo("quietoIzq",377,1573,95,121);
-  sonic->setRectangulo("quietoIzq",472,1573,95,121);
-  sonic->setRectangulo("quietoIzq",567,1573,95,121);
+  sonic->setRectangulo("quietoIzq",94,1573,97,121);
+  sonic->setRectangulo("quietoIzq",191,1573,97,121);
+  sonic->setRectangulo("quietoIzq",288,1573,91,121);
+  sonic->setRectangulo("quietoIzq",379,1573,95,121);
+  sonic->setRectangulo("quietoIzq",474,1573,95,121);
+  sonic->setRectangulo("quietoIzq",569,1573,95,121);
 
   sonic->setClip("correrDer");
   sonic->setRectangulo("correrDer",0,968,96,121);
   sonic->setRectangulo("correrDer",96,968,96,121);
-  sonic->setRectangulo("correrDer",192,968,96,121);
-  sonic->setRectangulo("correrDer",288,968,96,121);
+  sonic->setRectangulo("correrDer",192,968,98,121);
+  sonic->setRectangulo("correrDer",290,968,94,121);
   sonic->setRectangulo("correrDer",384,968,96,121);
   sonic->setRectangulo("correrDer",480,968,88,121);
   sonic->setRectangulo("correrDer",568,968,98,121);
-  sonic->setRectangulo("correrDer",666,968,95,121);
+  sonic->setRectangulo("correrDer",666,968,94,121);
 
   sonic->setClip("correrIzq");
   sonic->setRectangulo("correrIzq",0,1089,96,121);
   sonic->setRectangulo("correrIzq",96,1089,96,121);
-  sonic->setRectangulo("correrIzq",192,1089,96,121);
-  sonic->setRectangulo("correrIzq",288,1089,96,121);
+  sonic->setRectangulo("correrIzq",192,1089,98,121);
+  sonic->setRectangulo("correrIzq",290,1089,94,121);
   sonic->setRectangulo("correrIzq",384,1089,96,121);
   sonic->setRectangulo("correrIzq",480,1089,88,121);
   sonic->setRectangulo("correrIzq",568,1089,98,121);
-  sonic->setRectangulo("correrIzq",666,1089,95,121);
+  sonic->setRectangulo("correrIzq",666,1089,94,121);
 }
