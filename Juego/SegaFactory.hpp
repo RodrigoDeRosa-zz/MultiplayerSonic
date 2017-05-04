df07350ce0fd8dc128c084bed1f1bb90dd86bcbe
marcossch <schapiramarcos@gmail.com>
2017-05-04 18:46:14 -0300

    idem anterior

--- Juego/SegaFactory.hpp
+++ Juego/SegaFactory.hpp
@@ -11,8 +11,6 @@ using namespace std;
 class SegaFactory{
   private:
     void initClips(MovingSonic*);
-    //identificador de cuantos personajes se crearon para no repetir sus colores
-    int idPers;
     //vector con la dirreccion de los distintos sprites
     vector<string> imagenes;
   public:
