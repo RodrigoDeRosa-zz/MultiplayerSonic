597d699a87825f9f468fe8895191dcfc608db241
facundo <facundoiguerrero@gmail.com>
2017-05-06 09:18:27 -0300

    logger

--- Juego/ClipGroup.cpp
+++ Juego/ClipGroup.cpp
@@ -1,14 +1,23 @@
 #include "ClipGroup.hpp"
+#include "Logger2.hpp"
 
 ClipGroup::ClipGroup(){}
 
 void ClipGroup::addClip(string nombre){
   Clip* aux = new Clip();
+  if(aux==NULL){
+    string mensaje = "El clip de accion: ";
+    mensaje += nombre;
+    mensaje += "no se pudo inicializar \n";
+    Logger::getInstance().log(mensaje,MEDIO);
+  }
   clips[nombre]= aux;
 }
 
 void ClipGroup::addRectangulo(string nombre,int x, int y, int w, int h){
+
   Clip* aux = clips[nombre];
+
   aux->addRectangulo(x,y,w,h);
   //clips[nombre]->addRectangulo(x,y,w,h);
 }
