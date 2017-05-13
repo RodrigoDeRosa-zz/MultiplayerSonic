dc20f3bea27530326487b4577faa9c9eba0bde1e
facundo <facundoiguerrero@gmail.com>
2017-05-13 17:42:07 -0300

    ACTUALIZACION UPDATE JUEGO

--- Modelo/Player.hpp
+++ Modelo/Player.hpp
@@ -24,4 +24,19 @@ class Player{
 		void updateXY(float dirX, float dirY);
 		bool isConnected();
 		void setConnected(bool connected);
+
+	/* ACA TENEMOS QUE DEFINIR UNA FUNCION QUE SEA:
+	 * void afectarseCon( entidad );
+	 * Lo que hace esta funcion es calcular la colision
+	 * y si es necesario hacer que la entidad afecte
+	 * al personaje*/
+
+	/*ACA TENEMOS QUE DEFINIR OTRA FUNCION :
+	 * void aplicarEfecto( efecto );
+	 * Lo que hace esta funcion es modificar los
+	 * atributos del personaje de acuerdo al tipo
+	 * de efecto que recibe. Osea maneja el efecto,
+	 * y determina que atributo debe modificar, pero no
+	 * como debe modificarlo. Esto ultimo es responsabilidad
+	 * del efecto, este sabe si tiene que sumar o restar.*/
 };
