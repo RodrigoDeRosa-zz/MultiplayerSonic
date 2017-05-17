2b06ae92d6b6ecbee338393c31aa485d13d8e81b
Guido Trucco <gftrucco@gmail.com>
2017-05-17 09:14:52 -0300

    r

--- Control/CameraControl.cpp
+++ Control/CameraControl.cpp
@@ -21,14 +21,20 @@ float CameraControl::getPosition(){
 }
 
 bool CameraControl::moveCameraLeft(float newPosition,Model* model,string playerName){
-	if(model->otherPlayerInPosition(playerName,this->getRightEdge(),false)) return false;
+	if(model->otherPlayerInPosition(playerName,this->getRightEdge(),false)) {
+		printf("Hay un jugador en el borde derecho, seteo la posicion a la anterior\n");
+		return false;
+	}
 	this->position = newPosition-(this->width*MARGIN_FACTOR);
 	if(this->position < 0) this->position = 0;
 	return true;
 }
 
 bool CameraControl::moveCameraRight(float newPosition,Model* model, string playerName){
-	if(model->otherPlayerInPosition(playerName,this->getLeftEdge(),true)) return false;
+	if(model->otherPlayerInPosition(playerName,this->getLeftEdge(),true)){
+		printf("Hay un jugador en el borde izquierdo, seteo la posicion a la anterior\n");
+		return false;
+	}
 	this->position = newPosition-(this->width*(1-MARGIN_FACTOR))+CHARACTER_WIDTH;
 	if(this->position < 0) this->position = 0;
 	return true;
@@ -45,6 +51,7 @@ bool CameraControl::moveCamera(float newPosition,Model* model, string playerName
 	if(this->playerInCameraRange(newPosition)){
 		return true;
 	}
+	printf("Mover Camara para jugador: %s\n",playerName.c_str());
 	if(newPosition < this->getLeftEdge()) return this->moveCameraLeft(newPosition,model,playerName);
 	return this->moveCameraRight(newPosition,model,playerName);
 }
